2d7ae6419cc979f3cc7896c7fc998366239e4cfd
Marcus Hutchings <greywolf@nevermore.plus.com>
2019-02-09 14:33:04 +0000

    Added Tank Player Controller

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -8,3 +8,15 @@ ATank* ATankPlayerController::GetControlledTank() const
 	return Cast<ATank>(GetPawn());
 }
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("BeginPlay!"));
+	ATank* meep = GetControlledTank();
+	if (meep){
+		UE_LOG(LogTemp, Warning, TEXT("Tank, %s, Controlled!"), *meep->GetName());
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("No Tank Controlled!"));
+	}
+}
