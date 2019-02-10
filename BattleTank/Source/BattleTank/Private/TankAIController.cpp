5a05feec0cc10ba4edc969e40e3bbbbbc0aa1a37
Marcus Hutchings <greywolf@nevermore.plus.com>
2019-02-10 14:52:14 +0000

    Added AI Controller for Tanks

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+#include "../Public/TankAIController.h"
+#include "Engine/World.h"
+
+
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank * ATankAIController::GetPlayerTank() const
+{
+	APlayerController* playerController = GetWorld()->GetFirstPlayerController();
+	if (playerController)
+	{
+		APawn* activePawn = playerController->GetPawn();
+		if (activePawn)
+		{
+			return Cast<ATank>(activePawn);
+		}
+	}
+	return nullptr;
+}
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	ATank* meep = GetControlledTank();
+	if (meep) {
+		UE_LOG(LogTemp, Warning, TEXT("Tank, %s, AI Controlled!"), *meep->GetName());
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("No Tank AI Controlled!"));
+	}
+	ATank* playeyTank = GetPlayerTank();
+	if (playeyTank) {
+		UE_LOG(LogTemp, Warning, TEXT("Found Player Tank, %s!"), *playeyTank->GetName());
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Could not find player tank!"));
+	}
+}
