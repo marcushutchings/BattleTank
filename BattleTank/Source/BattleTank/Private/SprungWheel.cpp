92e083e66843d7b1511b1b8df885a0e6f5182364
Marcus Hutchings <greywolf@nevermore.plus.com>
2019-02-26 21:52:17 +0000

    Attached spring to Tank - not finished.

--- BattleTank/Source/BattleTank/Private/SprungWheel.cpp
+++ BattleTank/Source/BattleTank/Private/SprungWheel.cpp
@@ -10,20 +10,35 @@ ASprungWheel::ASprungWheel()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Spring = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("Spring Physics Constraint"));
+	SetRootComponent(Spring);
+
 	BodyMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("Body Mesh"));
-	SetRootComponent(BodyMesh);
+	BodyMesh->SetSimulatePhysics(true);
+	BodyMesh->SetupAttachment(Spring);
 
 	WheelMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("Wheel Mesh"));
-	WheelMesh->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+	WheelMesh->SetSimulatePhysics(true);
+	WheelMesh->SetupAttachment(Spring);
 
-	Spring = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("Spring Physics Constraint"));
-	Spring->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+
+
+	// Prevent any angular momentum
 	Spring->SetAngularSwing1Limit(EAngularConstraintMotion::ACM_Locked, 0.f);
 	Spring->SetAngularSwing2Limit(EAngularConstraintMotion::ACM_Locked, 0.f);
 	Spring->SetAngularTwistLimit(EAngularConstraintMotion::ACM_Locked, 0.f);
+
+	// Spring the wheel and body vertically
 	Spring->SetLinearPositionDrive(false, false, true);
 	Spring->SetLinearVelocityDrive(false, false, true);
+	Spring->SetLinearDriveParams(50.f, 20.f, 0.f);
 
+	// Allow the wheel to fall and rise separately.
+	Spring->SetLinearXLimit(ELinearConstraintMotion::LCM_Locked, 0.f);
+	Spring->SetLinearYLimit(ELinearConstraintMotion::LCM_Locked, 0.f);
+	Spring->SetLinearZLimit(ELinearConstraintMotion::LCM_Free, 0.f);
+
+	//Spring->SetConstrainedComponents(BodyMesh, NAME_None, WheelMesh, NAME_None);
 }
 
 // Called when the game starts or when spawned
@@ -31,6 +46,12 @@ void ASprungWheel::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (GetAttachParentActor())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("NOT NULL %s"), *GetAttachParentActor()->GetName());
+	}
+	else
+		UE_LOG(LogTemp, Warning, TEXT("NULL"));
 }
 
 // Called every frame
---
+++ BattleTank/Source/BattleTank/Public/SpawnPoint.h
@@ -0,0 +1,33 @@
+// Corpyright Marcus Hutchings 2019
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "SpawnPoint.generated.h"
+
+class ASprungWheel;
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BATTLETANK_API USpawnPoint : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	USpawnPoint();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+		TSubclassOf<AActor> SpawnClass;
+	
+};
